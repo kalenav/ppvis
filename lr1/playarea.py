725e71dd62c9f844643269883a924867940cf748
Konstantin <konstantin.loktev.02@mail.ru>
2022-04-02 11:57:21 +0300

    PlayArea class

---
+++ lr1/playarea.py
@@ -0,0 +1,33 @@
+#from train import Train
+
+class PlayArea:
+    def __init__(self, stations_graph_IN, trains_IN):
+        self.stations_graph = stations_graph_IN
+        self.trains = trains_IN
+
+    def getStations(self):
+        return self.stations
+
+    def getTrains(self):
+        return self.trains
+
+    def sendTrain(self, index):
+        self.trains[index].send()
+
+    def stopTrain(self, index):
+        self.trains[index].stop()
+        self.trains[index].setCurrStationType(self.trains[index].getCurrStation().getType())
+        self.trains[index].setCurrDistanceToDestination(self.trains[index].getCurrStation().getLinkWeight(self.trains[index].getCurrDestination()))
+
+    def nextTurn(self):
+        for index in len(self.trains):
+            train = self.trains[index]
+            if(train.isEnRoute()):
+                speed = train.getLocomotive().getSpeed()
+                train.reduceServiceTimeLeftBy(speed)
+                if(train.getServiceTimeLeft() < 0):
+                    train.disintegrate()
+                    continue
+                train.setCurrDistanceToDestination(train.getCurrDistanceToDestination() - speed)
+                if(train.getCurrDistanceToDestination() <= 0):
+                    self.stopTrain(index)
