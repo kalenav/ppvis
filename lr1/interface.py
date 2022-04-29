965d4ac4a4c50ab00c36236b88bb3e6e53e99bd2
Konstantin <konstantin.loktev.02@mail.ru>
2022-04-29 18:11:18 +0300

    command line parser ready(?)

--- lr1/interface.py
+++ lr1/interface.py
@@ -11,13 +11,35 @@ playarea = fr.readPlayareaFromFile()
 
 ui = UI()
 
+
 parser = argparse.ArgumentParser(description='Railroad simulation')
+
 subparsers = parser.add_subparsers()
+
 parser_printState = subparsers.add_parser('print', help='Print current playarea state')
 parser_printState.set_defaults(func=ui.printPlayareaState)
 
+parser_trainInfo = subparsers.add_parser('traininfo', help='Print selected train information')
+parser_trainInfo.add_argument('id', type=int)
+parser_trainInfo.set_defaults(func=ui.printTrainInfo)
+
+parser_sendTrain = subparsers.add_parser('send', help='Send selected train to its next destination')
+parser_sendTrain.add_argument('id', type=int)
+parser_sendTrain.set_defaults(func=ui.sendTrain)
+
+parser_loadTrain = subparsers.add_parser('load', help='Load selected train')
+parser_loadTrain.add_argument('id', type=int)
+parser_loadTrain.set_defaults(func=ui.loadTrain)
+
+parser_unloadTrain = subparsers.add_parser('unload', help='Unload selected train')
+parser_unloadTrain.add_argument('id', type=int)
+parser_unloadTrain.set_defaults(func=ui.unloadTrain)
+
 args = parser.parse_args()
-args.func(playarea)
+callbackArgs = [playarea]
+if hasattr(args, 'id'):
+    callbackArgs.append(args.id)
+args.func(*callbackArgs)
 
 fw = FileWriter(filename, playarea)
 fw.writePlayareaToFile()
