2c8f252f2eea14928dc52b9abebf88ec60e0f868
Konstantin <konstantin.loktev.02@mail.ru>
2022-05-21 12:29:56 +0300

    can now add entries

--- lr2/src/controller.py
+++ lr2/src/controller.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from view import AppScreenManager
 from storage import InMemoryStorage, XMLStorage
+from model import Student
 
 class Controller(App):
 
@@ -12,8 +13,11 @@ class Controller(App):
     def build(self):
         return self.screen_manager
 
-    def add_entry(self, student):
-        pass
+    def add_entry(self, name, course, group, completed, overall, lang):
+        data = self.storage.load()
+        data.append(Student(name, int(course), group, int(completed), int(overall), lang))
+        self.storage.save(data)
+        self.display_table()
 
     def find_entry(self, params_obj):
         pass
@@ -22,6 +26,10 @@ class Controller(App):
         pass
 
     def load(self, filename):
+        self.screen_manager.current_screen.display_table([])
         data = XMLStorage(filename[0]).load()
         self.storage.save(data)
+        self.display_table()
+
+    def display_table(self):
         self.screen_manager.current_screen.display_table(self.storage.load()[:10])
--- lr2/src/view.py
+++ lr2/src/view.py
@@ -2,11 +2,16 @@ ENTRY_LABEL_COUNT = 6
 WHITE = (1, 1, 1, 1)
 BLACK = (0, 0, 0, 1)
 SINGLE_PAGE_MAX_ENTRY_COUNT = 10
+BOTTOM_PADDING = 50
 
+from tkinter.font import BOLD
+from typing import Text
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.uix.actionbar import ActionBar
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.core.window import Window
@@ -20,11 +25,11 @@ class MainView(Screen):
     
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.add_widget(Table([]))
         self.add_widget(MainActionBar())
+        self.add_widget(Table([]))
     
     def display_table(self, students):
-        self.remove_widget(self.children[1])
+        self.remove_widget(self.children[0])
         self.add_widget(Table(students))
 
 
@@ -36,12 +41,22 @@ class FileChooserView(Screen):
         self.add_widget(FileChooserActionBar())
 
 
+class NewEntryView(Screen):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.add_widget(NewEntryPromptBlock())
+        self.add_widget(AddEntryActionBar())
+
+
+
 class AppScreenManager(ScreenManager):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_widget(MainView(name='main'))
         self.add_widget(FileChooserView(name='file_choose'))
+        self.add_widget(NewEntryView(name='new_entry'))
         
 
 
@@ -50,6 +65,7 @@ class Table(StackLayout):
     def __init__(self, students, **kwargs):
         super().__init__(**kwargs)  
         self.orientation = 'tb-lr'
+        self.padding = [0, 0, 0, BOTTOM_PADDING]
         for student in students:
             self.add_widget(Entry(student))
 
@@ -60,12 +76,13 @@ class Entry(BoxLayout):
         super().__init__(**kwargs)  
         self.orientation = 'horizontal'
         self.size_hint_y = 1 / SINGLE_PAGE_MAX_ENTRY_COUNT
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.name}'))
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.course}'))
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.group}'))
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.completed}'))
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.overall}'))
-        self.add_widget(EntryLabel(self.width / ENTRY_LABEL_COUNT, text=f'{student.lang}'))
+        ENTRY_WIDTH = self.width / ENTRY_LABEL_COUNT
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.name}'))
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.course}'))
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.group}'))
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.completed}'))
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.overall}'))
+        self.add_widget(EntryLabel(ENTRY_WIDTH, text=f'{student.lang}'))
 
 
 class EntryLabel(Label):
@@ -74,6 +91,7 @@ class EntryLabel(Label):
         super().__init__(**kwargs)
         self.width = width
         self.color = BLACK
+
 
 class MainActionBar(ActionBar):
 
@@ -85,3 +103,56 @@ class FileChooserActionBar(ActionBar):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+
+class AddEntryActionBar(ActionBar):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class NewEntryPromptBlock(GridLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.padding = [0, 0, 0, BOTTOM_PADDING]
+        self.rows = ENTRY_LABEL_COUNT
+        self.cols = 2
+
+        self.add_widget(NewEntryPromptLabel(text='Student name: '))
+        self.name = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(NewEntryPromptLabel(text='Course: '))
+        self.course = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.course)
+
+        self.add_widget(NewEntryPromptLabel(text='Group: '))
+        self.group = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.group)
+
+        self.add_widget(NewEntryPromptLabel(text='Completed: '))
+        self.completed = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.completed)
+
+        self.add_widget(NewEntryPromptLabel(text='Overall: '))
+        self.overall = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.overall)
+
+        self.add_widget(NewEntryPromptLabel(text='Programming language: '))
+        self.lang = NewEntryPromptTextInput(multiline=False)
+        self.add_widget(self.lang)
+
+
+class NewEntryPromptLabel(Label):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.height = 20
+        self.color = BLACK
+
+class NewEntryPromptTextInput(TextInput):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.height = 20
